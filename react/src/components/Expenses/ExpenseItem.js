d22dacef2d35de090947cae31e23f4ce9cf25bf0
welleh <kevinjiang64@hotmail.com>
2021-11-16 23:12:56 -0500

    new file

---
+++ react/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+function ExpenseItem (props) {
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = "Updated";
+        console.log(title);
+    }
+
+    return (
+    <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+    </Card> 
+    );
+}
+
+export default ExpenseItem;
